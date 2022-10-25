3b68a8c798c78a8e83df1153cdf67ef3349e5adf
mirtehouwing <mirtehouwing@gmail.com>
2022-10-25 13:33:20 +0200

    link to github repo

---
+++ src/nl/mirte/practice/Main.java
@@ -0,0 +1,22 @@
+package nl.mirte.practice;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+    }
+
+
+
+}
+
+
+// [] 1. Create a class which implements the following interface:
+// interface WordFrequency { }
+// [] 2. Create a class which implements the following interface:
+// interface WordFrequencyAnalyzer { }
+
+// [] 3. Implement the three methods defined in this interface.
+
+
